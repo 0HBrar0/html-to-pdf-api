85d65fafcb7d969f6588e051fa5ff1725b15c494
0hbrar0 <hbrar2004@gmail.com>
2026-02-25 00:23:02 -0800

    html to pdf api

---
+++ Procfile
@@ -0,0 +1,3 @@
+@"
+web: uvicorn main:app --host 0.0.0.0 --port 8080
+"@ | Set-Content -Path Procfile -Encoding UTF8
---
+++ main.py
@@ -0,0 +1,13 @@
+@"
+from fastapi import FastAPI, Request, Response
+from weasyprint import HTML
+
+app = FastAPI()
+
+@app.post("/html-to-pdf")
+async def convert_to_pdf(request: Request):
+    body = await request.json()
+    html_content = body.get("html")
+    pdf_bytes = HTML(string=html_content).write_pdf()
+    return Response(content=pdf_bytes, media_type="application/pdf")
+"@ | Set-Content -Path main.py -Encoding UTF8
